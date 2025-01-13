9c6244378e38bda44e1fbabe3860aa3e8b007a5f
Ahmed Amr <dev.ahmedamr989@gmail.com>
2025-01-13 11:40:15 +0200

    added eit/delete forms

--- modify_request.py
+++ modify_request.py
@@ -1,12 +1,11 @@
 import requests
 access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzM2MzQzMzgzLCJpYXQiOjE3MzYyNTY5ODMsImp0aSI6Ijc0N2YwODRmY2NkMjQ2YzU5YTg2YWU4NzU3YjBiODZiIiwidXNlcl9pZCI6NH0.xlja7uTgWSYLGrtlfT0JPk3o2fTlxCqkOGcoNoV984E"
 
-access_token2 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzM2Mzg0OTI1LCJpYXQiOjE3MzYyOTg1MjUsImp0aSI6IjFjY2IzMmJjM2FiZDQxMDJiNGQ5YWM5NWNmNTk0OTFhIiwidXNlcl9pZCI6NX0.Tku-LFS7KU1bmBeN2TCkbBQkW0XUTtxLycKqK4PuCYo"
+access_token2 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzM2NDk3Nzc1LCJpYXQiOjE3MzY0MTEzNzUsImp0aSI6Ijg2ZmJkZmIyYWEwNzQ3MGNhZTQwODZmMDhmZjdjMzU0IiwidXNlcl9pZCI6NX0.itDvy2-a8WFu_FYRXwhEJnOUhRfmuvDfFpaR8AAYgFU"
 headers = {
     "Authorization": f"Bearer {access_token2}"
     # Indicating that the data is in JSON format
 }
-url = "http://127.0.0.1:8000/api/edit-hotel/?hotel_name=Alex%20New%20Hotel"
-data = {"name": "New Hotel", "price": 1000}
-re = requests.put(url, data=data, headers=headers)
+url = "http://127.0.0.1:8000/user/user_created_hotels/"
+re = requests.get(url, headers=headers)
 print(re.status_code, re.content)
